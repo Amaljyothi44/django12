f1a905ac7e18353962fc7a91e3ab7669a99beefd
amaljyothi44 <amaljyothi414@gmail.com>
2023-07-02 13:26:37 +0530

    second

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1yt$mqftpn_=@5uaw#yxw*0^c%jbvvk8i0u)5pj00v_ye)w(um
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
